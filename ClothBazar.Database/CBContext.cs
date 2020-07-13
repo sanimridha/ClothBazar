e73549b6fa6225a4be1406bbc3a881ae079deafb
sanimridha <sanimridha307@gmail.com>
2020-07-13 12:07:40 +0600

    Add products and category business entities and created database

---
+++ ClothBazar.Database/CBContext.cs
@@ -0,0 +1,22 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Database
+{
+    class DBContext : DbContext
+    {
+        public DBContext() : base("ClothBazarConnection")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
